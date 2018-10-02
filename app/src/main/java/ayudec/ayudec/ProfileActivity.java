18ec5501f818e9a61f3b2e023a0c4e66fbbe93bd
didominguez <dominguezriveradiego@gmail.com>
2018-10-01 23:59:33 -0300

    Interfaz pantalla de perfil.

---
+++ app/src/main/java/ayudec/ayudec/ProfileActivity.java
@@ -0,0 +1,46 @@
+package ayudec.ayudec;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import com.allyants.chipview.ChipView;
+import com.allyants.chipview.SimpleChipAdapter;
+
+import java.util.ArrayList;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_activity_layout);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        ChipView cvTag = (ChipView)findViewById(R.id.especView);
+        ArrayList<Object> data = new ArrayList<>();
+        data.add("First Item");
+        data.add("Second Item");
+        data.add("Third Item");
+        data.add("Fourth Item");
+        data.add("Fifth Item");
+        data.add("Sixth Item");
+        data.add("Seventh Item");
+        SimpleChipAdapter adapter = new SimpleChipAdapter(data);
+        cvTag.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
