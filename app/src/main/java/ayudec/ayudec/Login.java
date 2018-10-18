6bc6dc3fc8163995819a0140a7779f4e3f01a5ce
Diego Varas <davarasm@gmail.com>
2018-10-18 19:29:08 -0300

    se agrego la clase GlobalVariable la que permite mantener activa la conexión del usuario,ademas se corrigio el volver a la pantalla de login solo volviendo atras y para desloguear se debe apretar boton en el menú de Perfil.

--- app/src/main/java/ayudec/ayudec/Login.java
+++ app/src/main/java/ayudec/ayudec/Login.java
@@ -9,8 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-
 
 public class Login extends AppCompatActivity {
     private Alumno _alumno;
